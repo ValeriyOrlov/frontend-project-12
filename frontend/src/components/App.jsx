f71be785d72b6e6eb61a5477d5b7b2f543aa678a
Valerrriy Orlov <wrestlbird@gmail.com>
2023-08-30 23:54:15 +0300

    added initial layout

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,4 +1,15 @@
+import { Navbar } from 'react-bootstrap';
 
-const App = () => {};
+const App = () => {
+  return (
+    <div className="d-flex flex-column h-100">
+      <Navbar expand="lg" className='shadow-sm bg-white'>
+        <div className="container">
+          <Navbar.Brand>Sidim - Buhtim</Navbar.Brand>  
+        </div>
+      </Navbar> 
+    </div>
+  )
+};
 
 export default App;
