9f8005fd98612cdec52ac79df0cfbe309e1a29c6
Valerrriy Orlov <wrestlbird@gmail.com>
2023-09-02 15:51:48 +0300

    add routes

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,14 +1,28 @@
 import { Navbar } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './LoginPage';
 
 const App = () => {
   return (
-    <div className="d-flex flex-column h-100">
-      <Navbar expand="lg" className='shadow-sm bg-white'>
-        <div className="container">
-          <Navbar.Brand>Sidim - Buhtim</Navbar.Brand>  
+    <Router>
+      <div className="d-flex flex-column h-100">
+        <Navbar expand="lg" className='shadow-sm bg-white'>
+          <div className="container">
+            <Navbar.Brand>Scvorechnik</Navbar.Brand>  
+          </div>
+        </Navbar>
+        <div className='container-fluid h-100'>
+          <div className='row justify-content-center align-content-center h-100'>
+            <div className='col-12 col-md-8 col-xxl-6'>
+            <Routes>
+              <Route path='/' element={null} />
+              <Route path='/login' element={<LoginPage />} />
+            </Routes>
+            </div>
+          </div>
         </div>
-      </Navbar> 
-    </div>
+      </div>
+    </Router>
   )
 };
 
