c03df4a2cfde75a4fc2e2e0017b8d1c8b0c0f5f2
Valerrriy Orlov <wrestlbird@gmail.com>
2023-09-03 18:13:26 +0300

    feat: redirect implementation and authorization checking

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,28 +1,64 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, 
+  Route, 
+  Routes,
+  Navigate,
+  useLocation
+} from 'react-router-dom';
+
 import { Navbar } from 'react-bootstrap';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './LoginPage';
+import ChatPage from './ChatPage';
+import AuthContext from '../contexts';
+import useAuth from '../hooks';
+
+const AuthProvider = ({ children }) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const logIn = () => setLoggedIn(true);
+  const logOut = () => {
+    setLoggedIn(false);
+  };
+
+  return <AuthContext.Provider value={{ loggedIn, logIn, logOut }}>
+    {children}
+  </AuthContext.Provider>
+}
+
+const ChatRoute = ({ children }) => {
+  const auth = useAuth();
+  const location = useLocation();
+  
+  return auth.loggedIn ? children : <Navigate to="/login" state={{ from: location }} />
+}
 
 const App = () => {
   return (
-    <Router>
-      <div className="d-flex flex-column h-100">
-        <Navbar expand="lg" className='shadow-sm bg-white'>
-          <div className="container">
-            <Navbar.Brand>Scvorechnik</Navbar.Brand>  
-          </div>
-        </Navbar>
-        <div className='container-fluid h-100'>
-          <div className='row justify-content-center align-content-center h-100'>
-            <div className='col-12 col-md-8 col-xxl-6'>
-            <Routes>
-              <Route path='/' element={null} />
-              <Route path='/login' element={<LoginPage />} />
-            </Routes>
+    <AuthProvider>
+      <Router>
+        <div className="d-flex flex-column h-100">
+          <Navbar expand="lg" className='shadow-sm bg-white'>
+            <div className="container">
+              <Navbar.Brand>Scvorechnik</Navbar.Brand>  
+            </div>
+          </Navbar>
+          <div className='container-fluid h-100'>
+            <div className='row justify-content-center align-content-center h-100'>
+              <div className='col-12 col-md-8 col-xxl-6'>
+              <Routes>
+                <Route path='/' element={(
+                  <ChatRoute>
+                    <ChatPage />
+                  </ChatRoute>                
+                )} 
+              />
+                <Route path='/login' element={<LoginPage />} />
+              </Routes>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </AuthProvider>
   )
 };
 
--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -2,9 +2,18 @@ import { useFormik } from 'formik';
 import { Form, FloatingLabel, Button } from 'react-bootstrap';
 import * as Yup from 'yup';
 import React, { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
+import routes from '../routes';
+import useAuth from '../hooks';
+import { useLocation, useNavigate } from 'react-router-dom';
+
 
 const LoginPage = () => {
+  const auth = useAuth();
+  const [authFailed, setAuthFailed] = useState(false);
   const inputRef = useRef();
+  const location = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
     inputRef.current.focus();
@@ -17,10 +26,26 @@ const LoginPage = () => {
         .max(15, 'Must be 15 characters or less')
         .required('Required'),
       password: Yup.string()
-        .min(6, 'must be 6 characters or more')
         .required('Required'),
     }),
-    onSubmit: (values) => console.log(values),
+    onSubmit: async (values) => {
+      setAuthFailed(false);
+      try {
+        const response = await axios.post(routes.loginPath(), values);
+        auth.logIn();
+        localStorage.setItem('userId', JSON.stringify(response.data));
+        const { from } = location.state;
+        navigate(from)
+      } catch (err) {
+        formik.setSubmitting(false);
+        if (err.isAxiosError && err.response.status === 401) {
+          setAuthFailed(true);
+          inputRef.current.select();
+          return;
+        }
+        throw err;
+      }
+    },
   });
 
   return (
@@ -34,6 +59,7 @@ const LoginPage = () => {
           className="col-12 col-md-6 mt-3 mt-mb-0"
         >
           <h1 className='text-center mb-4'>Войти</h1>
+          <fieldset disabled={formik.isSubmitting}>
             <FloatingLabel
               controlId='username'
               label='Ваш ник'
@@ -43,6 +69,7 @@ const LoginPage = () => {
                 name="username"
                 autoComplete='username'
                 placeholder='Ваш ник'
+                isInvalid={authFailed}
                 required
                 ref={inputRef}
                 {...formik.getFieldProps('username')}
@@ -60,15 +87,18 @@ const LoginPage = () => {
                 name='password'
                 autoComplete='password'
                 type="text" 
-                placeholder="Пароль" 
+                placeholder="Пароль"
+                isInvalid={authFailed}
                 required
                 {...formik.getFieldProps('password')}
               />
                 {formik.touched.password && formik.errors.password ? (
                 <div>{formik.errors.password}</div>
                 ) : null}
+                <Form.Control.Feedback type="invalid">the username or password is incorrect</Form.Control.Feedback>
             </FloatingLabel>
             <Button type="submit" variant="outline-primary" className='w-100'>Войти</Button>
+          </fieldset>
         </Form>
       </div>
       <div className='card-footer p-4'></div>
