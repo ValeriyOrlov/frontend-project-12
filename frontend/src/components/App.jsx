f9c218e1dba3b46a16db4ecf3436fb909960c6ed
Valerrriy Orlov <wrestlbird@gmail.com>
2023-09-12 23:32:00 +0300

    feat: added: exit button, route and registration page, header design

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -6,24 +6,52 @@ import { BrowserRouter as Router,
   useLocation
 } from 'react-router-dom';
 
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Button, Image } from 'react-bootstrap';
 import LoginPage from './LoginPage';
 import ChatPage from './ChatPage';
 import AuthContext from '../contexts';
 import useAuth from '../hooks';
+import SignupPage from './SignupPage';
 
 const AuthProvider = ({ children }) => {
   const [loggedIn, setLoggedIn] = useState(false);
   const logIn = () => setLoggedIn(true);
   const logOut = () => {
+    localStorage.removeItem('userId');
     setLoggedIn(false);
   };
 
   return <AuthContext.Provider value={{ loggedIn, logIn, logOut }}>
     {children}
   </AuthContext.Provider>
-}
+};
 
+const AuthButton = () => {
+  const auth = useAuth();
+
+  return (
+    auth.loggedIn
+      ? <Button onClick={auth.logOut}>Выйти</Button>
+      : null
+  );
+};
+
+const LogoImg = () => {
+  const auth = useAuth();
+
+  return (
+    auth.loggedIn
+      ? <Image 
+      src='../../images/auth_success_logo.jpg'
+      style={{ width: '78px' }}
+      ></Image>
+      : <Image 
+      src='../../images/logo.jpg'
+      style={{ width: '58px' }}
+      ></Image>
+  );
+};
+ 
 const ChatRoute = ({ children }) => {
   const auth = useAuth();
   const location = useLocation();
@@ -32,30 +60,34 @@ const ChatRoute = ({ children }) => {
 }
 
 const App = () => {
+
   return (
     <AuthProvider>
       <Router>
-        <div className="d-flex flex-column h-100">
+        <div className='d-flex flex-column h-100'>
           <Navbar expand="lg" className='shadow-sm bg-white'>
             <div className="container">
-              <Navbar.Brand>Scvorechnik</Navbar.Brand>  
+              <Navbar.Brand>
+                <LogoImg />
+                  Scvorechnik
+              </Navbar.Brand>
+              <Image 
+                src='../../images/birds_on_navbar.jpg'
+                style={{ height: '64px' }}
+              />
+              <AuthButton />
             </div>
           </Navbar>
-          <div className='container-fluid h-100'>
-            <div className='row justify-content-center align-content-center h-100'>
-              <div className='col-12 col-md-8 col-xxl-6'>
-              <Routes>
-                <Route path='/' element={(
-                  <ChatRoute>
-                    <ChatPage />
-                  </ChatRoute>                
-                )} 
-              />
-                <Route path='/login' element={<LoginPage />} />
-              </Routes>
-              </div>
-            </div>
-          </div>
+          <Routes>
+            <Route path='/' element={(
+              <ChatRoute>
+                <ChatPage />
+              </ChatRoute>                
+              )} 
+            />
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/signup' element={<SignupPage />} />
+          </Routes>
         </div>
       </Router>
     </AuthProvider>
