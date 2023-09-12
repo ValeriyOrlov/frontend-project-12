be4f6b4cdb5caa0368b6b8e636016012edf27f62
Valerrriy Orlov <wrestlbird@gmail.com>
2023-09-12 23:41:19 +0300

    feat: message rendering is implemented

---
+++ frontend/src/components/Messages.jsx
@@ -0,0 +1,20 @@
+import { useSelector } from "react-redux";
+import Message from "./Message";
+
+const Messages = () => {
+  const { messages } = useSelector((state) => state.messagesInfo);
+  const { currentChannelId } = useSelector((state) => state.channelsInfo);
+  const messagesFromCurrentChannel = messages
+  .filter(({ message }) => message.channelId === currentChannelId);
+  
+  return (
+    <>
+      {
+        messagesFromCurrentChannel
+        .map(({ message }) => <Message key={message.id} message={message} />)
+      }
+    </>
+  );
+};
+
+export default Messages;
