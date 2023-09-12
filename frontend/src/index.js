ac629ae0a5e9fe0c6ed7ec6bc3aba02f0a14c387
Valerrriy Orlov <wrestlbird@gmail.com>
2023-09-12 23:24:34 +0300

    init store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App.jsx';
+import { Provider } from 'react-redux';
+import store from './slices/index'
 
 const root = ReactDOM.createRoot(document.getElementById('chat'));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
