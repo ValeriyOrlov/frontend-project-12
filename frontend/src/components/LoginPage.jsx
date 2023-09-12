95d6c1458b01f014de0d2159ecb962e6a2b4f366
Valerrriy Orlov <wrestlbird@gmail.com>
2023-09-12 23:27:02 +0300

    ref: removed unnecessary validation, added image

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -1,6 +1,5 @@
 import { useFormik } from 'formik';
 import { Form, FloatingLabel, Button } from 'react-bootstrap';
-import * as Yup from 'yup';
 import React, { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 import routes from '../routes';
@@ -21,13 +20,6 @@ const LoginPage = () => {
 
   const formik = useFormik({
     initialValues: { username: '', password: ''},
-    validationSchema: Yup.object({
-      username: Yup.string()
-        .max(15, 'Must be 15 characters or less')
-        .required('Required'),
-      password: Yup.string()
-        .required('Required'),
-    }),
     onSubmit: async (values) => {
       setAuthFailed(false);
       try {
@@ -49,59 +41,69 @@ const LoginPage = () => {
   });
 
   return (
-    <div className='card shadow-sm'>
-      <div className='card-body row p-5'>
-        <div className='col-12 col-md-6 d-flex align-items-center justify-content-center'>
-          <img className="rounded-circle" src="" alt="" />
+    <div className='containter-fluid h-100'>
+      <div className='row justify-content-center align-content-center h-100'>
+        <div className='col-12 col-md-8 col-xxl-6'>
+          <div className='card shadow-sm'>
+            <div className='card-body row p-5'>
+              <div className='col-12 col-md-6 d-flex align-items-center justify-content-center'>
+                <img 
+                  className="rounded-circle" 
+                  style={{ width: '300px'}}
+                  src="../../images/auth_bird.jpg" 
+                  alt="auth_bird" />
+              </div>
+              <Form
+                onSubmit={formik.handleSubmit}
+                className="col-12 col-md-6 mt-3 mt-mb-0"
+                >
+                <h1 className='text-center mb-4'>Войти</h1>
+                <fieldset disabled={formik.isSubmitting}>
+                  <FloatingLabel
+                    controlId='username'
+                    label='Ваш ник'
+                    className='mb-3'
+                    >
+                    <Form.Control
+                      name="username"
+                      autoComplete='username'
+                      placeholder='Ваш ник'
+                      isInvalid={authFailed}
+                      required
+                      ref={inputRef}
+                      {...formik.getFieldProps('username')}
+                    />
+                  </FloatingLabel>
+                  <FloatingLabel
+                    controlId="floatingInput"
+                    label="Пароль"
+                    className="mb-3"
+                    >
+                    <Form.Control 
+                      name='password'
+                      autoComplete='password'
+                      type="password" 
+                      placeholder="Пароль"
+                      isInvalid={authFailed}
+                      required
+                      {...formik.getFieldProps('password')}
+                    />
+                    <Form.Control.Feedback type="invalid">the username or password is incorrect</Form.Control.Feedback>
+                  </FloatingLabel>
+                  <Button type="submit" variant="outline-primary" className='w-100'>Войти</Button>
+                </fieldset>
+              </Form>
+            </div>
+            <div className='card-footer p-4'>
+              <div className='text-center'>
+                <span>Нет аккаунта?</span>
+                {' '}
+                <a href="/signup">Регистрация</a>
+              </div>
+            </div>
+          </div>
         </div>
-        <Form
-          onSubmit={formik.handleSubmit}
-          className="col-12 col-md-6 mt-3 mt-mb-0"
-        >
-          <h1 className='text-center mb-4'>Войти</h1>
-          <fieldset disabled={formik.isSubmitting}>
-            <FloatingLabel
-              controlId='username'
-              label='Ваш ник'
-              className='mb-3'
-            >
-             <Form.Control
-                name="username"
-                autoComplete='username'
-                placeholder='Ваш ник'
-                isInvalid={authFailed}
-                required
-                ref={inputRef}
-                {...formik.getFieldProps('username')}
-              />
-               {formik.touched.username && formik.errors.username ? (
-                <div>{formik.errors.firstName}</div>
-                ) : null}
-            </FloatingLabel>
-            <FloatingLabel
-              controlId="floatingInput"
-              label="Пароль"
-              className="mb-3"
-            >
-              <Form.Control 
-                name='password'
-                autoComplete='password'
-                type="text" 
-                placeholder="Пароль"
-                isInvalid={authFailed}
-                required
-                {...formik.getFieldProps('password')}
-              />
-                {formik.touched.password && formik.errors.password ? (
-                <div>{formik.errors.password}</div>
-                ) : null}
-                <Form.Control.Feedback type="invalid">the username or password is incorrect</Form.Control.Feedback>
-            </FloatingLabel>
-            <Button type="submit" variant="outline-primary" className='w-100'>Войти</Button>
-          </fieldset>
-        </Form>
       </div>
-      <div className='card-footer p-4'></div>
     </div>
   )
 }
