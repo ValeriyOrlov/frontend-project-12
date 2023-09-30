1b42fec6513fb3065e34e85e452c4d9737674978
Valerrriy Orlov <wrestlbird@gmail.com>
2023-09-30 13:47:18 +0300

    feat: added Russian and English locales

---
+++ frontend/src/locales/en.js
@@ -0,0 +1,36 @@
+const en = {
+  translation: {
+      "key_one": "{{count}} message",
+      "key_other": "{{count}} messages",
+      "login": "Sign in",
+      "logout": "LogOut",
+      "nickname": "Login",
+      "password": "Password",
+      "no_account": "New in the birdhouse?",
+      "sign_up": "Sign up",
+      "username": "Username",
+      "confirm_pswrd": "Confirm password",
+      "delete": "Delete",
+      "rename": "Rename",
+      "channels": "Channels",
+      "enter_a_message": "Type a message...",
+      "add_channel": "Add channel",
+      "rename_the_channel": "Rename the channel",
+      "delete_channel": "Delete channel",
+      "confirmation": "Are you shure?",
+      "channel_name": "Channel name",
+      "cancel": "Cancel",
+      "send": "Send",
+      "remove": "Remove",
+      "from_3_to_20 characters": "from 3 to 20 characters",
+      "required_field": "Required field",
+      "at_least_6_characters": "at least 6 characters",
+      "passwords_must_not_match": "passwords must not match",
+      "such_a_user_already_exists": "such a user already exists",
+      "error": 'Error',
+      "Incorrect_username_and_password": "Incorrect username and password",
+
+  },
+};
+
+export default en;
---
+++ frontend/src/locales/index.js
@@ -0,0 +1,6 @@
+import ru from './ru.js';
+import en from './en.js';
+
+const resources = { en, ru };
+
+export default resources;
