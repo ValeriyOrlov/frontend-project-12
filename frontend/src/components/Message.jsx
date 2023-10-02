cce873825d97f8cbd8a702d0daec9a13f1b54558
Valerrriy Orlov <wrestlbird@gmail.com>
2023-10-02 23:02:08 +0300

    feat: added filtering of obscene messages in Russian and English languages

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,9 +1,15 @@
+import filter from 'leo-profanity';
+
 const Message = ({ message }) => {
+  filter.add(filter.getDictionary('en'))
+  filter.add(filter.getDictionary('ru'))
+
+  const clearedMsg = filter.clean(message.body);
   return (
     <div className="text-break mb-2">
       <b>{message.username}</b>
       :{' '}
-      {message.body}
+      {clearedMsg}
     </div>
   )
 }
