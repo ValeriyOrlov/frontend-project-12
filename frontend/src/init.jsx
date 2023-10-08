c9961d40c68208db3ecc3d686bb9167faee800c1
Valerrriy Orlov <wrestlbird@gmail.com>
2023-10-08 14:41:37 +0300

    language detector added

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,10 +1,12 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import socket from './socket';
 import resources from './locales/index';
 
 const init = () => {
   i18next
+    .use(LanguageDetector)
     .use(initReactI18next)
     .init({
       debug: true,
